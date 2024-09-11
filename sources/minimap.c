e2979d9addae34c1ae6edbc2db71471c857d7f11
iez-zagh <ezzaghba38@gmail.com>
2024-09-11 20:03:46 +0100

    done with put the map

--- sources/minimap.c
+++ sources/minimap.c
@@ -6,7 +6,7 @@
 /*   By: iez-zagh <iez-zagh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 16:30:26 by iez-zagh          #+#    #+#             */
-/*   Updated: 2024/09/11 15:23:47 by iez-zagh         ###   ########.fr       */
+/*   Updated: 2024/09/11 17:42:50 by iez-zagh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,30 +24,38 @@ int	checking_collision2(t_data *data, float x, float y)
 
 void	draw_direction(t_data *data, float x, float y)
 {
-	float dir_x, dir_y;
+	float	dir_x;
+	float	dir_y;
+	float	i;
 
-	dir_x = cos(data->player->angle);
+	dir_x = cos(data->player->angle); //cos(0) = 1 | sin(0) = 0
 	dir_y = sin(data->player->angle);
-	for (float i = 0; i < 26; i++)
+	i = RADIUS;
+	while (i < 26)
 	{
 		if (!checking_collision2(data, x + (dir_x * i), y + (dir_y * i)))
 			mlx_put_pixel(data->img, x + (dir_x * i), y + (dir_y * i), BLACK);
+		i++;
 	}
 }
 
 void	remove_direction(t_data *data, float x, float y)
 {
-	float dir_x, dir_y;
+	float	dir_x;
+	float	dir_y;
+	float	i;
 
-	dir_x = cos(data->player->old_angle);
+	dir_x = cos(data->player->old_angle); //cos(0) = 1 | sin(0) = 0
 	dir_y = sin(data->player->old_angle);
-	for (float i = 4; i < 26; i++)// Ensure the entire line is cleared
+	i = RADIUS;
+	while (i < 26)
 	{
 		if (!checking_collision2(data, x + (dir_x * i), y + (dir_y * i)))
 			mlx_put_pixel(data->img, x + (dir_x * i), y + (dir_y * i), WHITE);
+		i++;
 	}
 }
- 
+
 void	remove_direction2(t_data *data, float x, float y)
 {
 	float dir_x, dir_y;
