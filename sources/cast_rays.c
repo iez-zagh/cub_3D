7a83c54519e7bf0bbacd2e5e532e433c97143ec3
iez-zagh <ezzaghba38@gmail.com>
2024-09-14 16:01:05 +0100

    done with one side

--- sources/cast_rays.c
+++ sources/cast_rays.c
@@ -6,7 +6,7 @@
 /*   By: iez-zagh <iez-zagh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 11:29:49 by iez-zagh          #+#    #+#             */
-/*   Updated: 2024/09/14 15:29:15 by iez-zagh         ###   ########.fr       */
+/*   Updated: 2024/09/14 16:00:43 by iez-zagh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@ void	cast_rays(t_data *data)
 	}
 	data->cast_angle = data->player->angle;
 	i = 0;
-	// while(i < 150)
-	// {
-	// 	data->cast_angle -= M_PI / 1100;
-	// 	cast_lines(data, data->player->sqaure_x, data->player->sqaure_y);
-	// 	i++;
-	// }
+	while(i < 150)
+	{
+		data->cast_angle -= M_PI / 1100;
+		cast_lines(data, data->player->sqaure_x, data->player->sqaure_y);
+		i++;
+	}
 	data->cast_angle = data->player->angle;
 }
 
@@ -42,7 +42,7 @@ void	remove_rays(t_data *data)
 	i = 0;
 	 //check how to save the angle  to cast and rotate
 	// data->cast_angle = data->player->angle;
-	printf("%f]\n", data->cast_angle);
+	// printf("%f]\n", data->cast_angle);
 	while(i < 150)
 	{
 		data->cast_angle += M_PI / 1100;
@@ -54,11 +54,11 @@ void	remove_rays(t_data *data)
 	// data->cast_angle = data->player->angle;
 	// angle = data->player->angle; //check how to save the angle  to cast and rotate
 	i = 0;
-	// while(i < 150)
-	// {
-	// 	data->cast_angle -= M_PI / 1100;
-	// 	remove_direction3(data, data->player->sqaure_x, data->player->sqaure_y);
-	// 	i++;
-	// }
-	// data->cast_angle = data->player->angle;
+	while(i < 150)
+	{
+		data->cast_angle -= M_PI / 1100;
+		remove_direction3(data, data->player->sqaure_x, data->player->sqaure_y);
+		i++;
+	}
+	data->cast_angle = data->player->angle;
 }
