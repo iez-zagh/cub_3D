c0fd65f1016f51be3b36d3a1d00399343e50dcf2
iez-zagh <ezzaghba38@gmail.com>
2024-09-21 18:22:06 +0100

    making

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: iez-zagh <iez-zagh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 14:02:03 by iez-zagh          #+#    #+#             */
-/*   Updated: 2024/09/21 18:06:26 by iez-zagh         ###   ########.fr       */
+/*   Updated: 2024/09/21 18:14:36 by iez-zagh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@
 # define WHITE 0xFFFFFFFF
 # define BLACK 0x00000FF
 # define TILE 32
-# define MOVE_SPEED 1.0
+# define MOVE_SPEED 5.0
 # define RADIUS 4.0
 # define RADIUS2 3.0
 # define ROTATE_ANGLE 0.05233333333
