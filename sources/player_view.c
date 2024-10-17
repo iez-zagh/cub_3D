55e1e2c6e264ff2ab6c7bee2126f75e04b3481f0
iez-zagh <ezzaghba38@gmail.com>
2024-10-17 01:05:10 +0100

    render player review

--- sources/player_view.c
+++ sources/player_view.c
@@ -6,7 +6,7 @@
 /*   By: iez-zagh <iez-zagh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 14:56:18 by iez-zagh          #+#    #+#             */
-/*   Updated: 2024/10/17 00:44:15 by iez-zagh         ###   ########.fr       */
+/*   Updated: 2024/10/17 01:04:10 by iez-zagh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,21 @@ void	player_view(t_data *data)
 {
 	int	i;
 	int	j;
+	int	color;
 
 	i = 0;
 	while (i < HEIGHT)
 	{
 		j = 0;
+		color = WHITE;
+		if (data->map[j / 32][i / 32] == '1')
+			color = BLACK;
 		while (j < WIDTH)
 		{
-			mlx_put_pixel(data->player_img, j, i, RED);
+			mlx_put_pixel(data->player_img, j, i, color);
 			j++;
 		}
 		i++;
 	}
-	
+		
 }
