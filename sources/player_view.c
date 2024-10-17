9ef4019796356cca2e2d6552d0e98f6d2e9105ae
iez-zagh <ezzaghba38@gmail.com>
2024-10-17 18:14:57 +0100

    render player view

--- sources/player_view.c
+++ sources/player_view.c
@@ -6,7 +6,7 @@
 /*   By: iez-zagh <iez-zagh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 14:56:18 by iez-zagh          #+#    #+#             */
-/*   Updated: 2024/10/17 01:04:10 by iez-zagh         ###   ########.fr       */
+/*   Updated: 2024/10/17 12:38:22 by iez-zagh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,12 @@ void	player_view(t_data *data)
 		j = 0;
 		color = WHITE;
 		if (data->map[j / 32][i / 32] == '1')
-			color = BLACK;
+			color = RED;
 		while (j < WIDTH)
 		{
 			mlx_put_pixel(data->player_img, j, i, color);
 			j++;
 		}
 		i++;
-	}
-		
+	}	
 }
