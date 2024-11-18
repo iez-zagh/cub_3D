e3149a536ae81beebfaa79f97a0dc107bd7c7974
Zakaria Maghdaoui <zmaghdao@e1r7p10.1337.ma>
2024-11-18 15:54:46 +0100

    updating atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -3,38 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iez-zagh <iez-zagh@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zmaghdao <zmaghdao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 18:31:28 by iez-zagh          #+#    #+#             */
-/*   Updated: 2023/11/17 17:02:27 by iez-zagh         ###   ########.fr       */
+/*   Updated: 2024/11/18 15:52:52 by zmaghdao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_check(const char *str, int *sign)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			*sign *= -1;
+		i++;
+	}
+	return (i);
+}
+
 int	ft_atoi(const char *str)
 {
 	int		i;
-	int		sign;
 	long	res;
+	int		sign;
+	long	tmp;
 
-	i = 0;
-	sign = 1;
 	res = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign = -sign;
-		i++;
-	}
+	sign = 1;
+	i = ft_check(str, &sign);
 	while (ft_isdigit(str[i]))
 	{
-		res = res * 10 + (str[i] - '0');
-		if (res < 0 && sign == 1)
+		tmp = res;
+		res = (res * 10) + (str[i] - 48);
+		if ((tmp != res / 10) && sign == 1)
 			return (-1);
-		if (res < 0 && sign == -1)
+		else if ((tmp != res / 10) && sign == -1)
 			return (0);
 		i++;
 	}
